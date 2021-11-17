50225ade854abb1cf40f81f3893433d258e404c2
Dan <dbarnett@hotmail.com>
2021-11-17 13:58:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# Automate Login Process 
+# user:automated  pw:automatedautomated
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+def get_driver():
+  #options to make browsing easier
+  options = webdriver.ChromeOptions()
+  #info-bars may pop up and confuse cursor location
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
+  options.add_argument("disable-blink-features=AutomationControlled")
+
+  driver = webdriver.Chrome(options=options)
+  driver.get("http://automated.pythonanywhere.com/login")
+  return driver
+
+def main():
+  driver = get_driver()
+  #xpath can be found by using inspect on the 
+  #web page to locate the item of interest
+  driver.find_element(by="id", value="id_username").send_keys("automated")
+  time.sleep(2)
+  driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+  time.sleep(2)
+  # click 'home' button
+  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+  
+
+print(main())
+
+  
